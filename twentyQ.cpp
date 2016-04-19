3e25e5756bc98b3a5635959e883563b970501a4d
Tarah-Peltz <tarah.peltz@colorado.edu>
2016-04-19 01:12:11 -0600

    Create twentyQ.cpp

---
+++ twentyQ.cpp
@@ -0,0 +1,40 @@
+#include "twentyQ.h"
+#include <string>
+#include <fstream>
+#include <sstream>
+using namespace std;
+
+
+
+Questions::Questions()
+{
+	//ctor
+}
+
+
+Questions::~Questions()
+{
+}
+
+void Questions::printMenu() {
+
+}
+void Questions::printDirections() {
+
+}
+void Questions::printQuestions() { //This function will display questions
+}
+
+void Questions::buildQuestions() {
+
+}
+
+void Questions::buildResponse() {
+
+}
+
+
+void Questions::addCount() { //adds count to pet struct
+	
+}
+
