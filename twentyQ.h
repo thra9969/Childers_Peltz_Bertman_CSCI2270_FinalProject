170c27f68bf0e9b5bfa2aefc3d84b304fbaf52cc
Tarah-Peltz <tarah.peltz@colorado.edu>
2016-04-21 12:25:55 -0600

    Update twentyQ.h

--- twentyQ.h
+++ twentyQ.h
@@ -8,14 +8,11 @@ struct Pets {
 	bool ansArr[20]; //answers to question for each pet
 	Pets *next;
 	int score = 0; //the score each pet has;  let the greatest pet win!
-	Pets * Head; //Points to head of list
-	//Pets * Tail; //Gets set by findPetsTail();
+
 };
 struct Questions {
 	std::string question; //what it asks
 	Questions *nextQ; //points to next question in list
-	Questions *Head; // Points to head in list
-	Questions *Temp; //Used to traverse through the questions
 };
 
 class Questions
@@ -25,14 +22,19 @@ public:
 	~Questions();
 	//Pets(); //Do we need to add this in?
 	//~Pets(); 
-	//void findPetsTail(); //Finds and sets tail of pets
 	void printMenu();
 	void printDirections();
 	void printQuestions(); //This function will display question and store answers
 	void addCount(); //adds count to pet struct
+	void findPetsTail(); //Finds tail of list
+	void buildPets(); //Builds pets
 	bool userArray[20]; //Used to store user responses
 	bool matchup; //Used to compare user responses with preset response in Pets array
 	int index; //Used to store current index of userArray
+	Pets * Phead; //Points to head of list
+	Pets * Ptail; //Gets set by findPetTail();
+	Questions *Qhead; // Points to head in list
+	Questions *Temp; //Used to traverse through the questions
 	
 private:
 	
