e196460aabe58c72dc4c323d26c91a5575bdc8ed
tich1726 <tich1726@colorado.edu>
2016-04-25 21:00:26 -0600

    Update twentyQ.cpp

--- twentyQ.cpp
+++ twentyQ.cpp
@@ -6,17 +6,17 @@
 using namespace std;
 
 
-Questions::Questions()
+twentyQ::Questions()
 {
 	//ctor
 }
 
 
-Questions::~Questions()
+twentyQ::~Questions()
 {
 }
 
-void Questions::readPets(){ //hope this works, no idea...
+void twentyQ::readPets(){ //hope this works, no idea...
 	//reading txt file containing pet names and answer values
 	string filename = "pets.txt";
 	string line = "";
@@ -24,26 +24,26 @@ void Questions::readPets(){ //hope this works, no idea...
 	infile.open(filename);
 	if(infile.is_open()){
 		string name = "";
-		int ans = 0;
+		string ans[20];
 		
 		while(!ifstream.eof()){
-			int i=0;
+			int i=-1;
 			getline(infile, line);
 			stringstream ss(line);
 			while (getline(ss, in, ',')){
-				if(i == 0){
+				if(i == -1){
 					name = in;
 				}else{
-					ans = atoi(in);
+					ans[i] = in;
 				}
-				q.addPets(name, ans);  //need to create class q and set function
 				i++;
 			}
+			q.addPets(name, ans);  //need to create class q and set function
 			
 		}
 	}
 }
-void Questions::readQuestions(){
+void twentyQ::readQuestions(){
 	string filename = "questions.txt";
 	string line = "";
 	ifstream infile;
@@ -56,7 +56,7 @@ void Questions::readQuestions(){
 	}
 }
 
-void Questions::printMenu() {
+void twentyQ::printMenu() {
 	cout<< "===== 20 QUESTIONS ===="<<endl;
 	cout<< "1. Directions"<<endl;
 	cout<< "2. Start"<<endl;
@@ -64,7 +64,7 @@ void Questions::printMenu() {
 	cout<< "4. Quit"<<endl;
 }
 
-void Questions::printDirections() { //A fun way to make it more interesting and change the outcome
+void twentyQ::printDirections() { //A fun way to make it more interesting and change the outcome
 	cout<<"Welcome to 20 questions. How are you doing?"<<endl;
 	string response;
 	getline(cin, response);
@@ -81,7 +81,7 @@ void Questions::printDirections() { //A fun way to make it more interesting and 
 	cout<<"I'm going to read your mind. I'll ask you 20 questions just to make sure I got a good read."<<endl;
 	cout<<"Then I'll tell you exactly what you were thinking of. Answer my questions with a yes or no"<<endl; 
 }
-void Questions::printQuestions() { //This function will display questions
+void twentyQ::printQuestions() { //This function will display questions
 	cout<<Temp->question<<endl;
 	getline(cin, userResponse);
 	Temp=Temp->nextQ;
@@ -90,18 +90,18 @@ void Questions::printQuestions() { //This function will display questions
 
 
 
-void Questions::buildQuestions(string question) {
+void twentyQ::buildQuestions(string question) {
 	index = 0;
 	//Set Qhead = question1 once it is built
 	
 
 }
 
-void Questions::buildPets (string name, int ans) { //Builds pets linked list. 
+void twentyQ::buildPets (string name, string ans) { //Builds pets linked list. 
 	
 }
 
-void Questions::findPetsTail() {
+void twentyQ::findPetsTail() {
 	Pets * Traverse = new Pets;
 	Traverse = Phead;
 	while (Traverse->next != NULL) {
@@ -110,7 +110,7 @@ void Questions::findPetsTail() {
 	PTail = Traverse;
 }
 
-void Questions::buildResponse() {//Might be " instead of '. I forget which symbol to use for a string. 
+void twentyQ::buildResponse() {//Might be " instead of '. I forget which symbol to use for a string. 
 	if ((userResponse =="yes") ||(userResponse =="Yes")||(userResponse =="y")||(userResponse =="Y")) {
 		matchup = true;// allows us to compare response to preset responses in addCount
 		userArray[index] = true;
@@ -125,7 +125,7 @@ void Questions::buildResponse() {//Might be " instead of '. I forget which symbo
 }
 
 
-void Questions::addCount() { //adds count to pet struct
+void twentyQ::addCount() { //adds count to pet struct
 	Pets * traverse = new Pets;
 	traverse = Phead 
 	if (matchup = traverse->ansArr[index-1]) //Has to be -1 because we incremented index in buildResponse
