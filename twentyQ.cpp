07c7e0f972c283f57a36c8f2852708b27c2fa5dc
abertman <alex.bertman@colorado.edu>
2016-04-26 18:44:51 -0600

    Update twentyQ.cpp

--- twentyQ.cpp
+++ twentyQ.cpp
@@ -1,68 +1,75 @@
 #include "twentyQ.h"
+#include <iostream>
+#include <stdlib.h>
 #include <string>
 #include <fstream>
 #include <sstream>
-#include <iostream>
+#include <istream>
+
 using namespace std;
 
-
-twentyQ::Questions()
+ifstream infile;
+twentyQ::twentyQ()
 {
 	//ctor
 }
 
 
-twentyQ::~Questions()
+twentyQ::~twentyQ()
 {
 }
 
-void twentyQ::buildHeads(){ //this function initializes the heads of both linked lists and sets walker pointers for both
-	//Questions * Qhead = new Questions;
-	//Pets * Phead = new Pets;
-	Qhead->next = NULL; 
+void twentyQ::buildHeads() { //this function initializes the heads of both linked lists and sets walker pointers for both
+							 //Questions * Qhead = new Questions;
+						 //Pets * Phead = new Pets;
+	int Dog[20] = {1, 1, 0, 0, 1, 1, 0, 0,1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0}
+	Phead->name = "Dog";
+	Phead->ansArr = Dog;
+	Qhead->nextQ = NULL;
 	Phead->next = NULL;
 	//Questions * Qwalk = new Questions;
 	Qwalk = Qhead; //need to add these to public
-	//Pets * Pwalk = new Pets;
+				   //Pets * Pwalk = new Pets;
 	Pwalk = Phead;
 }
 
-void twentyQ::readPets(){ //hope this works, no idea...
-	//reading txt file containing pet names and answer values
+void twentyQ::readPets() { //hope this works, no idea...
+						   //reading txt file containing pet names and answer values
 	string filename = "pets.txt";
 	string line = "";
-	ifstream infile;
+	
 	infile.open(filename);
-	if(infile.is_open()){
+	if (infile.is_open()) {
 		string name = "";
 		string ans[20];
-		
-		while(!infile.eof()){
-			int i=-1;
+
+		while (!infile.eof()) {
+			int i = -1;
 			string in;
 			getline(infile, line);
 			stringstream ss(line);
-			while (getline(ss, in, ',')){
-				if(i == -1){
+			while (getline(ss, in, ',')) {
+				if (i == -1) {
 					name = in;
-				}else{
-					
-					ans[i] = atoi(in);
+				}
+				else {
+
+					ans[i] = stoi(in);
 				}
 				i++;
 			}
 			q.addPets(name, ans);  //need to create class q and set function
-			
+
 		}
 	}
 }
-void twentyQ::readQuestions(){
+void twentyQ::readQuestions() {
 	string filename = "questions.txt";
 	string line = "";
-	ifstream infile;
+	
 	infile.open(filename);
-	if(infile.is_open()){
-		while(!infile.eof()){
+	if (infile.is_open()) {
+		while (!infile.eof()) {
 			getline(infile, line);
 			q.addQuestions(line);
 		}
@@ -70,34 +77,34 @@ void twentyQ::readQuestions(){
 }
 
 void twentyQ::printMenu() {
-	cout<< "===== 20 QUESTIONS ===="<<endl;
-	cout<< "1. Directions"<<endl;
-	cout<< "2. Start"<<endl;
-	cout<< "3. ?"<<endl;
-	cout<< "4. Quit"<<endl;
+	cout << "===== 20 QUESTIONS ====" << endl;
+	cout << "1. Directions" << endl;
+	cout << "2. Start" << endl;
+	cout << "3. ?" << endl;
+	cout << "4. Quit" << endl;
 }
 
 void twentyQ::printDirections() { //A fun way to make it more interesting and change the outcome
-	cout<<"Welcome to 20 questions. How are you doing?"<<endl;
+	cout << "Welcome to 20 questions. How are you doing?" << endl;
 	string response;
 	getline(cin, response);
 	if (response.size() % 3 = 1) {
-		cout<<"Well, your day is about to get a whole lot better!"<<endl;
+		cout << "Well, your day is about to get a whole lot better!" << endl;
 	}
 	else if (response.size() % 3 = 2) {
-		cout<<"Well, mine has been awful. Thanks for asking."<<endl;
+		cout << "Well, mine has been awful. Thanks for asking." << endl;
 	}
 	else {
-		cout<<"Oh. I don't actually care. Sorry. People don't get my sarcasm."<<endl;
+		cout << "Oh. I don't actually care. Sorry. People don't get my sarcasm." << endl;
 	}
-	cout<<"Here's how to play. Think of a pet. This pet has to be a pet legal to own in America. So no kangaroos."<<endl;
-	cout<<"I'm going to read your mind. I'll ask you 20 questions just to make sure I got a good read."<<endl;
-	cout<<"Then I'll tell you exactly what you were thinking of. Answer my questions with a yes or no"<<endl; 
+	cout << "Here's how to play. Think of a pet. This pet has to be a pet legal to own in America. So no kangaroos." << endl;
+	cout << "I'm going to read your mind. I'll ask you 20 questions just to make sure I got a good read." << endl;
+	cout << "Then I'll tell you exactly what you were thinking of. Answer my questions with a yes or no" << endl;
 }
 void twentyQ::printQuestions() { //This function will display questions
-	cout<<Qwalk->question<<endl;
+	cout << Qwalk->question << endl;
 	getline(cin, userResponse);
-	Qwalk=Qwalk->nextQ;
+	Qwalk = Qwalk->nextQ;
 }
 
 
@@ -112,8 +119,8 @@ void twentyQ::buildQuestions(string question) {
 
 }
 
-void twentyQ::buildPets (string name, int ans[]) { //Builds pets linked list. //Might need ans[20] 
-	//this seems really simple but I think it works. I have a walker pointer set and then updated to the next (originally set as head)
+void twentyQ::buildPets(string name, int ans[]) { //Builds pets linked list. //Might need ans[20] 
+												  //this seems really simple but I think it works. I have a walker pointer set and then updated to the next (originally set as head)
 	Pets * NewP = new Pets;
 	NewP = name;
 	NewP->ansArr = ans;
@@ -131,14 +138,14 @@ void twentyQ::findPetsTail() {
 }
 
 void twentyQ::buildResponse() {//Might be " instead of '. I forget which symbol to use for a string. 
-	if ((userResponse =="yes") ||(userResponse =="Yes")||(userResponse =="y")||(userResponse =="Y")) {
-		matchup = true;// allows us to compare response to preset responses in addCount
-		userArray[index] = true;
+	if ((userResponse == "yes") || (userResponse == "Yes") || (userResponse == "y") || (userResponse == "Y")) {
+		matchup = 1;// allows us to compare response to preset responses in addCount
+		userArray[index] = 1;
 		index++;
 	}
-	if ((userResponse =="no") ||(userResponse =="No")||(userResponse =="n")||(userResponse =="N")) {
-		matchup = false;
-		userArray[index] = false;
+	if ((userResponse == "no") || (userResponse == "No") || (userResponse == "n") || (userResponse == "N")) {
+		matchup = 0;
+		userArray[index] = 0;
 		index++;
 	}
 
@@ -147,12 +154,12 @@ void twentyQ::buildResponse() {//Might be " instead of '. I forget which symbol 
 
 void twentyQ::addCount() { //adds count to pet struct
 	Pets * traverse = new Pets;
-	traverse = Phead 
-	if (matchup = traverse->ansArr[index-1]) //Has to be -1 because we incremented index in buildResponse
-	{
-		traverse->score = traverse->score+1;		
-	}
+	traverse = Phead
+		if (matchup = traverse->ansArr[index - 1]) //Has to be -1 because we incremented index in buildResponse
+		{
+			traverse->score = traverse->score + 1;
+		}
 	traverse = traverse->next;
-	
+
 }
 
