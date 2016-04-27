4ccee6db9d5f2582bf1ddcf73351894355650182
tich1726 <tich1726@colorado.edu>
2016-04-27 03:56:33 -0600

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -4,18 +4,30 @@
 #include <string>
 #include <fstream>
 #include <sstream>
-#include <istream>
-
+#include <iostream>
+#include <vector>
 
 using namespace std;
 
 
 //int main(int argc, char * argv[]) {
-int main () {
+int main() {
 	twentyQ q;
-	q.readPets();
-	q.readQuestions();
-	q.buildHeads();
+	/*Pets * Phead = new Pets; //Points to head of list
+	Pets * Ptail = new Pets; //Gets set by findPetTail();
+	Ptail = q.findPetsTail();
+	Questions *Qhead; // Points to head in list
+	Questions *Qwalk; //Used to traverse through the questions
+	Pets *Pwalk;
+	int Dog[20] = { 1, 1, 0, 0, 1, 1, 0, 0,1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0 };
+	Phead->name = "Dog";
+	Phead->ansArr = Dog;
+	Qhead->nextQ = NULL;
+	Phead->next = NULL;*/
+
+	//q.readPets();
+	//q.readQuestions();
+	//q.buildHeads();
 	int a = 1;
 	while (a = 1) {
 		string input = "";
@@ -27,18 +39,24 @@ int main () {
 
 		switch (choice)
 		{
-		case 1: //print directions
+		case 1: { //print directions
+			q.printDirections();
+			break; }
+		case 2: {
 			q.printQuestions();
+			break; }
+		case 3: {
 			break;
-		case 2: 
-			q.printQuestions();
+		}
+
+		case 4: {
+			cout << "Goodbye!" << endl;
+			exit(0); }
+
+		default:{
+			cout << "Invalid input. Please try again." << endl;
 			break;
-
-		case 4:	cout << "Goodbye!" << endl;
-			exit(0);
-
-		default: break;
-
+			}
 		}
 
 	}
